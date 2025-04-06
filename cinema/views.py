ca7a670d40817928eba7658799bae1f0eb6d9b53
Valentin Simonov <simonov0101k@gmail.com>
2025-04-06 20:01:16 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -41,7 +41,7 @@ class GenreList(APIView):
 
 class GenreDetailList(APIView):
     @staticmethod
-    def get_object(pk: int) -> Response:
+    def get_object(pk: int) -> Genre:
         return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk: int) -> Response:
